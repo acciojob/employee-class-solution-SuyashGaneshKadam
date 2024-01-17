526569f83170d2b8353c2d17e1237977baaddee1
Suyash Kadam <122480150+SuyashGaneshKadam@users.noreply.github.com>
2024-01-18 00:46:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-function Employee(name, salary) {
-  this.name = name;
-  this.salary = salary;
+class Employee {
+  constructor(name, salary) {
+    this.name = name;
+    this.salary = salary;
+  }
+
+  giveRaise(amount) {
+    this.salary += amount;
+  }
 }
 
-Employee.prototype.giveRaise = function(amount) {
-  this.salary += amount;
-}
+class Manager extends Employee {
+  constructor(name, salary, department) {
+    super(name, salary);
+    this.department = department;
+  }
 
-function Manager(name, salary, department) {
-  Employee.call(this, name, salary);
-  this.department = department;
-}
-
-Manager.prototype = Object.create(Employee.prototype);
-Manager.prototype.constructor = Manager;
-
-Manager.prototype.giveRaise = function(amount) {
-  Employee.prototype.giveRaise.call(this, amount);
-  console.log(`New salary for ${this.name} in ${this.department}: ${this.salary}`);
+  giveRaise(amount) {
+    super.giveRaise(amount);
+    console.log(`New salary for ${this.name} in ${this.department}: ${this.salary}`);
+  }
 }
